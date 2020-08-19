d9828cec7ac301ac42053d86b6e816facefdf930
Stuart Lynn <50170698+sllynn@users.noreply.github.com>
2020-08-19 18:29:49 +0100

    Add basic unit tests to module (#3)
    * Added some basic unit tests

---
+++ sparkxgb/testing/utils.py
@@ -0,0 +1,28 @@
+import pyspark
+from pyspark.sql.session import SparkSession
+
+SPARK_SCALA_MAPPING = {
+    "2": "2.11",
+    "3": "2.12"
+}
+
+def default_session(conf=None):
+    spark_major_version = pyspark.__version__[0]
+    scala_version = SPARK_SCALA_MAPPING[spark_major_version]
+    mvn_group = "ml.dmlc"
+    xgb_version = "1.0.0"
+    xgboost4j_coords = f"{mvn_group}:xgboost4j_{scala_version}:{xgb_version}"
+    xgboost4j_spark_coords = f"{mvn_group}:xgboost4j-spark_{scala_version}:{xgb_version}"
+
+    if conf is None:
+        conf = {
+            "spark.jars.packages": ",".join([xgboost4j_coords, xgboost4j_spark_coords])
+        }
+
+    builder = SparkSession.builder.appName("spark-xgboost")
+    for key, value in conf.items():
+        builder = builder.config(key, value)
+
+    session = builder.getOrCreate()
+
+    return session

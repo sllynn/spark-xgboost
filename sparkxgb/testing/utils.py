b741ed81734f65bfaf3e27ba7e69738e3054829d
sllynn <stuart@databricks.com>
2020-08-19 16:57:26 +0100

    Added some basic unit tests

---
+++ sparkxgb/testing/utils.py
@@ -0,0 +1,14 @@
+from pyspark.sql.session import SparkSession
+
+
+def default_session(conf=None):
+    if conf is None:
+        conf = {"spark.jars.packages": "ml.dmlc:xgboost4j_2.11:1.0.0,ml.dmlc:xgboost4j-spark_2.11:1.0.0"}
+
+    builder = SparkSession.builder.appName("spark-xgboost")
+    for key, value in conf.items():
+        builder = builder.config(key, value)
+
+    session = builder.getOrCreate()
+
+    return session
---
+++ sparkxgb/tests/classifier.py
@@ -0,0 +1,82 @@
+import unittest
+
+from pyspark.sql.types import StringType
+
+from sparkxgb.xgboost import XGBoostClassifier
+from sparkxgb.testing.utils import default_session
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler, StringIndexer, Binarizer
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+
+class XGBClassifierTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.spark = default_session()
+
+        col_names = [
+            "age", "workclass", "fnlwgt",
+            "education", "education-num",
+            "marital-status", "occupation",
+            "relationship", "race", "sex",
+            "capital-gain", "capital-loss",
+            "hours-per-week", "native-country",
+            "label"
+        ]
+
+        sdf = (
+            self.spark.read
+                .csv(path="./data/adult.data", inferSchema=True)
+                .toDF(*col_names)
+                .repartition(200)
+        )
+
+        string_columns = [fld.name for fld in sdf.schema.fields if isinstance(fld.dataType, StringType)]
+        string_col_replacements = [fld + "_ix" for fld in string_columns]
+        string_column_map = list(zip(string_columns, string_col_replacements))
+        target = string_col_replacements[-1]
+        predictors = [fld.name for fld in sdf.schema.fields if
+                      not isinstance(fld.dataType, StringType)] + string_col_replacements[:-1]
+
+        si = [StringIndexer(inputCol=fld[0], outputCol=fld[1]) for fld in string_column_map]
+        va = VectorAssembler(inputCols=predictors, outputCol="features")
+        pipeline = Pipeline(stages=[*si, va])
+        fitted_pipeline = pipeline.fit(sdf)
+
+        sdf_prepared = fitted_pipeline.transform(sdf)
+
+        self.train_sdf, self.test_sdf = sdf_prepared.randomSplit([0.8, 0.2], seed=1337)
+
+    def test_binary_classifier(self):
+
+        self.spark.sparkContext.setLogLevel("INFO")
+
+        xgb_params = dict(
+            eta=0.1,
+            maxDepth=2,
+            missing=0.0,
+            objective="binary:logistic",
+            numRound=5,
+            numWorkers=2
+        )
+
+        xgb = (
+            XGBoostClassifier(**xgb_params)
+                .setFeaturesCol("features")
+                .setLabelCol("label_ix")
+        )
+
+        bce = BinaryClassificationEvaluator(
+            rawPredictionCol="rawPrediction",
+            labelCol="label_ix"
+        )
+
+        model = xgb.fit(self.train_sdf.limit(10))
+        roc = bce.evaluate(model.transform(self.test_sdf))
+
+        print(roc)
+
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()

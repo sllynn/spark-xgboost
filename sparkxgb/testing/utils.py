d7d4f6399677766a81a16e7265845903e80379e7
sllynn <stuart@databricks.com>
2020-08-19 18:25:21 +0100

    updated testing utils function to pick appropriate jars for spark version

--- sparkxgb/testing/utils.py
+++ sparkxgb/testing/utils.py
@@ -1,9 +1,23 @@
+import pyspark
 from pyspark.sql.session import SparkSession
 
+SPARK_SCALA_MAPPING = {
+    "2": "2.11",
+    "3": "2.12"
+}
 
 def default_session(conf=None):
+    spark_major_version = pyspark.__version__[0]
+    scala_version = SPARK_SCALA_MAPPING[spark_major_version]
+    mvn_group = "ml.dmlc"
+    xgb_version = "1.0.0"
+    xgboost4j_coords = f"{mvn_group}:xgboost4j_{scala_version}:{xgb_version}"
+    xgboost4j_spark_coords = f"{mvn_group}:xgboost4j-spark_{scala_version}:{xgb_version}"
+
     if conf is None:
-        conf = {"spark.jars.packages": "ml.dmlc:xgboost4j_2.12:1.0.0,ml.dmlc:xgboost4j-spark_2.12:1.0.0"}
+        conf = {
+            "spark.jars.packages": ",".join([xgboost4j_coords, xgboost4j_spark_coords])
+        }
 
     builder = SparkSession.builder.appName("spark-xgboost")
     for key, value in conf.items():

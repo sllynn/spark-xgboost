0c3dd4bca33b886324177274c470b92fb3ec9f0a
sllynn <stuart@databricks.com>
2020-08-19 18:15:55 +0100

    switched to scala 2.12

--- sparkxgb/testing/utils.py
+++ sparkxgb/testing/utils.py
@@ -3,7 +3,7 @@ from pyspark.sql.session import SparkSession
 
 def default_session(conf=None):
     if conf is None:
-        conf = {"spark.jars.packages": "ml.dmlc:xgboost4j_2.11:1.0.0,ml.dmlc:xgboost4j-spark_2.11:1.0.0"}
+        conf = {"spark.jars.packages": "ml.dmlc:xgboost4j_2.12:1.0.0,ml.dmlc:xgboost4j-spark_2.12:1.0.0"}
 
     builder = SparkSession.builder.appName("spark-xgboost")
     for key, value in conf.items():
